64964b95cdfae469826580deb2dade1c1710ee13
Matt Lingwood <Matt.lingwood@uk.tesco.com>
2016-12-16 19:20:42 +0000

    Refactored FiveHourDisplay tests

--- src/test/java/ClockFigures/FiveHoursDisplayTest.java
+++ src/test/java/ClockFigures/FiveHoursDisplayTest.java
@@ -1,5 +1,6 @@
 package ClockFigures;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,69 +12,69 @@ import static org.hamcrest.core.Is.is;
  * Created by Matt on 16/12/2016.
  */
 public class FiveHoursDisplayTest {
+    private DisplayBase display;
+
+    @Before
+    public void init(){
+        display = new FiveHoursDisplay();
+    }
 
     @Test
     public void All_displays_are_off_by_default(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(0);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(0);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(0));
+        assertThat((CountActiveDisplays(activeDisplays)), is(0));
     }
 
     @Test
     public void Fifth_hour_first_display_only(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(5);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(5);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(1));
+        assertThat((CountActiveDisplays(activeDisplays)), is(1));
     }
     @Test
     public void Nineth_hour_first_display_only(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(9);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(9);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(1));
+        assertThat((CountActiveDisplays(activeDisplays)), is(1));
     }
 
     @Test
     public void Tenth_hour_returns_two_displays(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(10);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(10);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(2));
+        assertThat((CountActiveDisplays(activeDisplays)), is(2));
     }
 
     @Test
     public void fifteenth_hour_returns_three_displays(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(15);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(15);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(3));
+        assertThat((CountActiveDisplays(activeDisplays)), is(3));
     }
 
     @Test
     public void twentieth_hour_returns_four_displays(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(20);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(20);
-
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(4));
+        assertThat((CountActiveDisplays(activeDisplays)), is(4));
     }
 
     @Test
     public void Nineth_tenth_eleventh_return_one_two_two(){
-        FiveHoursDisplay displays = new FiveHoursDisplay();
+        List<DisplayNode> activeDisplays = display.activateDisplays(9);
+        int countOne = CountActiveDisplays(activeDisplays);
+        List<DisplayNode> activeDisplaysTwo = display.activateDisplays(10);
+        int countTwo = CountActiveDisplays(activeDisplaysTwo);
+        List<DisplayNode> activeDisplaysThree = display.activateDisplays(11);
+        int countThree = CountActiveDisplays(activeDisplaysThree);
 
-        List<DisplayNode> activeDisplays = displays.activateDisplays(9);
-        assertThat(((int) activeDisplays.stream().filter(display -> display.isOn()).count()), is(1));
-        List<DisplayNode> activeDisplaysTwo = displays.activateDisplays(10);
-        assertThat(((int) activeDisplaysTwo.stream().filter(display -> display.isOn()).count()), is(2));
-        List<DisplayNode> activeDisplaysThree = displays.activateDisplays(11);
-        assertThat(((int) activeDisplaysThree.stream().filter(display -> display.isOn()).count()), is(2));
+        assertThat(countOne, is(1));
+        assertThat(countTwo, is(2));
+        assertThat(countThree, is(2));
+    }
+
+    private int CountActiveDisplays(List<DisplayNode> activeDisplays) {
+        return (int) activeDisplays.stream().filter(display -> display.isOn()).count();
     }
 }
